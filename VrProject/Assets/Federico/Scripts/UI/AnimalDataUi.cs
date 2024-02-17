9bb2af7d483958a018a5003d2c0acc81c2a14d86
Federico <s306156@studenti.polito.it>
2024-02-17 11:20:05 +0100

    [ADD] aggiunto modo per sostiuire l'immagine con i cheat e con il progresso o il completamento di una missione

--- VrProject/Assets/Federico/Scripts/UI/AnimalDataUi.cs
+++ VrProject/Assets/Federico/Scripts/UI/AnimalDataUi.cs
@@ -16,6 +16,8 @@ public class AnimalDataUi : QuestEventReceiver
     public string[] description = new string[3];
     //Descrizione delle missioni degli animali 
     public int index=3;
+    
+    public bool imageChanged=true;
     // immagine da visualizzare
     public Sprite AnimalTitleImage;
     public Sprite AnimalSprite;
@@ -27,11 +29,18 @@ public class AnimalDataUi : QuestEventReceiver
             if(eventType==EventType.AREA_ENTER)
             {
                currAnimalImage=AnimalSprite;
+               imageChanged=true;
               
             }
             else if(eventType==EventType.PROGRESS)
             {
                Debug.Log(quest.GetQuestDescription());
+               //per aggiornare l'immagine
+               if(!imageChanged)
+               {
+                currAnimalImage=AnimalSprite;
+                imageChanged=true;
+               }
               
             }
             else if(eventType==EventType.COMPLETE)
@@ -41,6 +50,12 @@ public class AnimalDataUi : QuestEventReceiver
                 if(index<3)
                 {
                     index++;
+                    //per aggiornare l'immagine
+                    if(!imageChanged)
+                    {
+                        currAnimalImage=AnimalSprite;
+                        imageChanged=true;
+                    }
                 }
                 //disicrivi dagli eventi(?) se mi iscrivo o no, la quest
                 SetEventSubscription(false,quest,EventType.AREA_ENTER);
