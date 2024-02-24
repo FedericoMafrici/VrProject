cc683c0c346f48abc069433af6ca854878d31244
Pietro Siliani <s314518@studenti.polito.it>
2024-02-24 08:14:34 +0100

    [NEW] Inizio costruzione script per path

---
+++ VrProject/Assets/Pietro/Scripts/Utility/PathArrow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathArrow : MonoBehaviour {
+    public PathArrow Next;
+    public bool Islast = false;
+
+
+
+
+}
